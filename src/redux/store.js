85991b77c04a4814d27503ad93423091698cc6c7
jinali98 <jinalipabasara@gmail.com>
2021-11-28 19:04:54 +0530

    updated store

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,8 +8,10 @@ import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const middleWares = [logger, thunk, sagaMiddleware];
-
+const middleWares = [thunk, sagaMiddleware];
+if (process.env.NODE_ENV === "development") {
+  middleWares.push(logger);
+}
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
 sagaMiddleware.run(rootSaga);
